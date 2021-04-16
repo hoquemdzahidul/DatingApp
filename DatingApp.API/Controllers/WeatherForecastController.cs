449eecdc762a34b2cdd64391ba54648cdb5770c1
Zahid <hoquemdzahidul@gmail.com>
2021-04-17 00:48:19 +0600

    Initial Commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private DataContext _dbContext;
+
+    
+
+      
+         public WeatherForecastController(DataContext dataContext)
+        {
+            this._dbContext = dataContext;
+            
+        }
+       
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            
+            var values =await _dbContext.Values.ToListAsync();
+
+            return Ok(values);
+        }
+    }
+}
